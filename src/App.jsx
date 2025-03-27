1f823b701dbf2a226d1fe62dd7930026252250bd
jerryspann <jerryspann0@gmail.com>
2025-03-27 16:05:13 -0700

    added current location

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 import * as weatherService from './services/weatherService';
 import WeatherSearch from './components/WeatherSearch/WeatherSearch';
 import WeatherDetails from './components/WeatherDetails/WeatherDetails';
-import './App.css'
+import './App.css';
 
 const App = () => {
   const [weather, setWeather] = useState({});
@@ -16,6 +16,34 @@ const App = () => {
     };
     setWeather(newWeatherState);
   };
+
+  useEffect(() => {
+
+    const fetchDefaultData = async (city) => {
+      const data = await weatherService.show(city);
+      const newWeatherState = {
+        location: data.location.name,
+        temperature: data.current.temp_f,
+        condition: data.current.condition.text,
+      };
+      setWeather(newWeatherState);
+    };
+
+    navigator.geolocation.getCurrentPosition(
+      function(result) {
+          const lat = result.coords.latitude;
+          const long = result.coords.longitude;
+          const locationString = `${lat}, ${long}`
+          fetchDefaultData(locationString);
+      }, function(error) {
+        console.log(error);
+      }
+    )
+    // Call the fetch fucntion when the page loads:
+    
+
+  }, []);
+  
 
   console.log ('State:', weather)
   return (
